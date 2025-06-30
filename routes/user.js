54d81f3811f12f8c7a508cbe7361948b32f8174d
Git-Ayush-Pandey <ayushpandeyb@gmail.com>
2025-06-30 22:57:24 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,27 @@ const passport = require("passport");
 const { savedUrl } = require("../middleware.js");
 const userController = require("../controller/users.js");
 
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner } = require("../middleware.js");
+const listingController = require("../controller/listings.js");
+
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
 router
   .route("/signup")
   .get(userController.renderSignupForm)
   .post(userController.signup);
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    wrapAsync(listingController.createListing)
+  );
 
 router
   .route("/login")
@@ -21,6 +38,7 @@ router
     }),
     userController.login
   );
+
 
 router.get("/logout", userController.logout);
 
